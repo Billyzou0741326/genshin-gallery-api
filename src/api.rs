11b9490f7d00adba2254300a3e9552502a0e33ae
Billy Zou <zguanhan@cecs.pdx.edu>
2022-02-11 01:49:37 +0000

    Add `/api/{name}` character specific endpoint

--- src/api.rs
+++ src/api.rs
@@ -1,5 +1,5 @@
 use actix_web::web::{Data, Query};
-use actix_web::{get, http, HttpResponse, Responder};
+use actix_web::{get, http, web, HttpResponse, Responder};
 use mongodb::Database;
 use serde::Deserialize;
 use serde_json::json;
@@ -33,6 +33,34 @@ pub async fn api_all(db: Data<Database>, Query(info): Query<ArtworkIdRequest>) -
         .build();
     let get_id_result = get_ids(&db, options).await;
     match get_id_result {
+        Ok(id_list) => HttpResponse::Ok()
+            .content_type("application/json")
+            .insert_header((http::header::ACCESS_CONTROL_ALLOW_ORIGIN, "*"))
+            .body(
+                json! ({
+                    "data": id_list,
+                })
+                .to_string(),
+            ),
+        Err(e) => HttpResponse::InternalServerError()
+            .insert_header((http::header::ACCESS_CONTROL_ALLOW_ORIGIN, "*"))
+            .body(e.to_string()),
+    }
+}
+
+#[get("/api/character/{name}")]
+pub async fn api_character_ids(
+    db: Data<Database>,
+    params: web::Path<(String,)>,
+    Query(info): Query<ArtworkIdRequest>,
+) -> impl Responder {
+    let (name,) = params.into_inner();
+    let art_type = info.art_type.unwrap_or_else(|| "SFW".to_owned());
+    let options = ArtworkQueryOption::builder()
+        .characters(vec![name])
+        .image_type(art_type)
+        .build();
+    match get_ids(&db, options).await {
         Ok(id_list) => HttpResponse::Ok()
             .content_type("application/json")
             .insert_header((http::header::ACCESS_CONTROL_ALLOW_ORIGIN, "*"))
