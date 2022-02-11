a3cfc34e1a1409ef72cca481efc1a45916ee4a92
Billy Zou <zguanhan@cecs.pdx.edu>
2022-02-11 01:47:27 +0000

    Add `/api/ids` endpoint

--- src/api.rs
+++ src/api.rs
@@ -1,8 +1,49 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::web::{Data, Query};
+use actix_web::{get, http, HttpResponse, Responder};
+use mongodb::Database;
+use serde::Deserialize;
+use serde_json::json;
+
+use crate::db::{get_ids, ArtworkQueryOption};
+
+#[derive(Deserialize)]
+pub struct ArtworkIdRequest {
+    #[serde(rename = "type")]
+    art_type: Option<String>,
+    character: Option<String>,
+}
 
 #[get("/api/health")]
 pub async fn api_health() -> impl Responder {
     HttpResponse::Ok()
         .content_type("application/json")
         .body("{}")
+}
+
+#[get("/api/all")]
+pub async fn api_all(db: Data<Database>, Query(info): Query<ArtworkIdRequest>) -> impl Responder {
+    let characters = match info.character {
+        Some(character) => vec![character],
+        None => vec![],
+    };
+    let art_type = info.art_type.unwrap_or_else(|| "SFW".to_owned());
+    let options = ArtworkQueryOption::builder()
+        .characters(characters)
+        .image_type(art_type)
+        .build();
+    let get_id_result = get_ids(&db, options).await;
+    match get_id_result {
+        Ok(id_list) => HttpResponse::Ok()
+            .content_type("application/json")
+            .insert_header((http::header::ACCESS_CONTROL_ALLOW_ORIGIN, "*"))
+            .body(
+                json! ({
+                    "data": id_list,
+                })
+                .to_string(),
+            ),
+        Err(e) => HttpResponse::InternalServerError()
+            .insert_header((http::header::ACCESS_CONTROL_ALLOW_ORIGIN, "*"))
+            .body(e.to_string()),
+    }
 }
